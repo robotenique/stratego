e0bbe7631eb474ed55ee52533ec225eb9eba23f2
juliano <juliano@JulianoDebian.home>
2016-05-16 16:15:40 -0300

    Finalmente conseguimos trocar de cena, porém só no INPUT D:

--- scn1.gd
+++ scn1.gd
@@ -4,12 +4,15 @@ extends Control
 # member variables here, example:
 # var a=2
 # var b="textvar"
+#var root = get_tree().get_root()
+#var current_scene = get_tree().get_current_scene()
+    
+func _ready():	
+	set_process_input(true)
+	set_fixed_process(true)
+	
 
-func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
-func _on_goto_scene_pressed():
-        get_node("/root/title1").goto_scene("res://regras.scn")
-
+func _input(event):
+	if(event.is_action("ui_accept")):		
+		get_tree().change_scene("res://regras.scn")
 
